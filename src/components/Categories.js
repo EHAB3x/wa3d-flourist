63d65c95a325105d88454add48b084f7e4f8894e
EHAB3x <anaelbob7288@gmail.com>
2023-08-17 00:44:35 +0200

    check

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import './components_css/Categories.css'
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCategories } from "../RTK/Slices/CategorySlice";
